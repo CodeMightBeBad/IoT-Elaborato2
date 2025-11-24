5556f0e78a72d247450bf5d27e5b65901f35fc06
Matt <matteo.crepaldi4@studio.unibo.it>
2025-11-24 11:10:57 +0100

    First version of the settings header file

---
+++ src/settings.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Leds
+#define ON_LED 10
+#define IN_ACTION_LED 9
+#define ALARM_LED 8
+
+// Buttons
+#define RESET_BUTTON 12
+
+// Sensors
+#define TEMP_SENSOR 11
+#define PRESENCE_DETECTOR 5
+#define SERVOMOTOR 4
+#define DISTANCE_DETECTOR 3
